02db0806103b69f5821b64f16d116883690a2670
Lim Boseul <limbs722@gmail.com>
2024-04-26 15:08:32 +0900

    updates
    @f-lab-jake 피드백 주신 부분 수정했습니다!

--- level_0/qrcode.ts
+++ level_0/qrcode.ts
@@ -1,9 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181903
 
 function qrcode(q: number, r: number, code: string) {
-    return [...code].map((item, idx) => {
-        if (idx % q === r) {
-            return item;
-        }
-    }).join('');
+    return [...code].filter((item, idx) => idx % q === r).join('');
 }
--- level_0/배열만들기5.ts
+++ level_0/배열만들기5.ts
@@ -1,11 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181911
 
 function makeArray5(intStrs: string[], k: number, s: number, l: number) {
-    return intStrs.reduce((acc: number[], value) => {
-        const compareNum: number = Number(value.slice(s, s + l));
-        if (compareNum > k) {
-            acc.push(compareNum)
-        }
-        return acc;
-    }, []);
+    return intStrs.map((item) => Number(item.slice(s, s + l))).filter((value) => value > k);
 }
