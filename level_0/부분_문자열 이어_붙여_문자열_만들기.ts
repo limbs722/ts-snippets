8ba7c43d6060e51adcc83cf69d60fa775e1e06af
Lim Boseul <limbs722@gmail.com>
2024-04-28 19:17:13 +0900

    updates

--- level_0/부분_문자열 이어_붙여_문자열_만들기.ts
+++ level_0/부분_문자열 이어_붙여_문자열_만들기.ts
@@ -1,6 +1,17 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181911
 
 function makeString(my_strings: string[], parts: number[][]) {
+
+    // lodash zip 메소드를 사용한 예시
+
+    //     import _ from 'lodash';
+
+    //     const zipArr = _.zip(my_strings, parts);
+    //     return zipArr.map((item) => {
+    //         const [s, e] = item[1];
+    //         return item[0].slice(s, e + 1);
+    //     }).join('');
+
     return my_strings.map((item, idx) => {
         const [s, e] = parts[idx];
         return item.slice(s, e + 1);
