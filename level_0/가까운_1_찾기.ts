f89375d625c10627d67c207da1f084addbe4c2f4
Lim Boseul <limbs722@gmail.com>
2024-04-29 15:52:50 +0900

    updates

--- level_0/가까운_1_찾기.ts
+++ level_0/가까운_1_찾기.ts
@@ -1,14 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181898
 
 function nearByOne(arr: number[], idx: number) {
-    let answer = -1;
-    
-    for (let i = idx; i < arr.length; i++) {
-        if (arr[i] === 1) {
-            answer = i;
-            break;
-        }
-    }
-    
-    return answer;
+    return arr.findIndex((v, i) => v === 1 && i >= idx) ?? -1;
 }
--- level_0/문자_개수_세기.ts
+++ level_0/문자_개수_세기.ts
@@ -1,16 +1,12 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181902
 
 function stringCount(my_string: string) {    
+    const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     const result = Array(52).fill(0);
-
-    for (let i = 0; i < my_string.length; i++) {
-        const alpha = my_string.charCodeAt(i);
-
-        if (alpha >= 65 && alpha <= 90) {
-            result[alpha-65]++;
-        } else{
-            result[alpha-71]++;
-        }
-    }
+    
+    [...my_string].forEach((i) => {
+        result[alpha.indexOf(i)] += 1;
+    });
+    
     return result;
 }
