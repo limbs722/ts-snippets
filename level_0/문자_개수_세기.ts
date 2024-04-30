620897d42bafcb14015e082ff73e1b8f34b9c390
Lim Boseul <limbs722@gmail.com>
2024-04-30 12:50:36 +0900

    Update 문자_개수_세기.ts

--- level_0/문자_개수_세기.ts
+++ level_0/문자_개수_세기.ts
@@ -2,7 +2,7 @@
 
 function stringCount(my_string: string) {    
     const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    const result = Array(52).fill(0);
+    const result = Array(alpha.length).fill(0);
     
     [...my_string].forEach((i) => {
         result[alpha.indexOf(i)] += 1;
