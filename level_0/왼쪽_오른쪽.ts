b0f90c4ff416ede499ad8ac18deb038d43d9676c
Lim Boseul <limbs722@gmail.com>
2024-05-02 16:24:42 +0900

    updates

--- level_0/왼쪽_오른쪽.ts
+++ level_0/왼쪽_오른쪽.ts
@@ -1,12 +1,11 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181890
 
 function leftRight(str_list: string[]) {
-    for (let i = 0; i < str_list.length; i++) {
-        if (str_list[i] === 'l') {
-            return str_list.slice(0, i);
-        }
-        if (str_list[i] === 'r') {
-            return str_list.slice(i + 1);
+    for (const [idx, ele] of str_list.entries()) {
+        if (ele === 'l') {
+            return str_list.slice(0, idx)
+        } else if (ele === 'r') {
+            return str_list.slice(idx + 1);
         }
     }
     
