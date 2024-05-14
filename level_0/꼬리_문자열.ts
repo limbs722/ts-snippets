2b3f38512b1dba20dd3c60b7b3dfb43bd145f5f9
Lim Boseul <limbs722@gmail.com>
2024-05-14 19:34:10 +0900

    updates

--- level_0/꼬리_문자열.ts
+++ level_0/꼬리_문자열.ts
@@ -1,7 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181841
 
 function stringTail(str_list: string[], ex: string) {
-    return str_list.reduce((acc, value) => {
-        return value.includes(ex) ? acc : acc + value;
-    }, '');
+    return str_list.filter((value) => !value.includes(ex)).join('');
 }
