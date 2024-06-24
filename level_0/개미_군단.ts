79a6a0c0cd5d5ef2e6fcc7649f2f0f23a0ec6840
Lim Boseul <limbs722@gmail.com>
2024-06-24 12:32:18 +0900

    Update 개미_군단.ts

--- level_0/개미_군단.ts
+++ level_0/개미_군단.ts
@@ -1,24 +1,18 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120837
 
 function antArmy(hp: number) {
-    const hpType = {
-        general: 5,
-        soldier: 3,
-        worker: 1
-    }
+    const antTypes = [
+        { name: 'general', hp: 5 },
+        { name: 'soldier', hp: 3 },
+        { name: 'worker', hp: 1 }
+    ];
 
     let remainHp = hp;
     let totalAnt = 0;
 
-    while (remainHp > 0) {
-        if (remainHp >= hpType.general) {
-            remainHp -= hpType.general;
-            totalAnt++;
-        } else if (remainHp >= hpType.soldier) {
-            remainHp -= hpType.soldier;
-            totalAnt++;
-        } else {
-            remainHp -= hpType.worker;
+    for (const antType of antTypes) {
+        while (remainHp >= antType.hp) {
+            remainHp -= antType.hp;
             totalAnt++;
         }
     }
