ea0372b8079bbbb7eb34a8bcf2bf9acf433f383e
Lim Boseul <limbs722@gmail.com>
2024-06-30 12:49:40 +0900

    updates

--- level_0/인덱스_바꾸기.ts
+++ level_0/인덱스_바꾸기.ts
@@ -1,16 +1,15 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120895
 
-function solution(my_string: string, num1: number, num2: number) {
-  const r1 = my_string.split('')[num1];
-  const r2 = my_string.split('')[num2];
-  
-  return [...my_string].map((v, i) => {
-      if (num1 === i) {
-          return r2;
-      } else if (num2 === i) {
-          return r1;
-      } else {
-          return v;
-      }
-  }).join('');
+function changeIndex(my_string: string, num1: number, num2: number) {
+    const r1 = my_string.split('')[num1];
+    const r2 = my_string.split('')[num2];
+    
+    return [...my_string].map((v, i) => {
+        if (num1 === i) {
+            return r2;
+        } else if (num2 === i) {
+            return r1;
+        }
+        return v;
+    }).join('');
 }
