5b8fc89c1d707352d4855746c5fcf0ea3ddc16f8
Lim Boseul <limbs722@gmail.com>
2024-07-08 17:39:51 +0900

    updates

--- level_0/인덱스_바꾸기.ts
+++ level_0/인덱스_바꾸기.ts
@@ -1,15 +1,8 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120895
 
 function changeIndex(my_string: string, num1: number, num2: number) {
-    const r1 = my_string.split('')[num1];
-    const r2 = my_string.split('')[num2];
+    const str = my_string.split('');
+    [str[num1], str[num2]] = [str[num2], str[num1]];
     
-    return [...my_string].map((v, i) => {
-        if (num1 === i) {
-            return r2;
-        } else if (num2 === i) {
-            return r1;
-        }
-        return v;
-    }).join('');
+    return str.join('');
 }
