6cb49e92bcb35a58cf55052e42ffc517f91857cb
Lim Boseul <limbs722@gmail.com>
2024-07-10 16:22:58 +0900

    updates

--- level_0/자릿수_더하기.ts
+++ level_0/자릿수_더하기.ts
@@ -1,6 +1,11 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120906
+import _ from 'lodash';
 
 function addToDigits(n: number) {
-  const strArr = String(n).split('');
-  return strArr.reduce((acc, value) => acc + Number(value), 0);
+  // lodash 사용
+  // return _.sum([...n.toString()]);
+  
+  return String(n).split("")
+        .map(Number)
+        .reduce((acc, value) => acc + value, 0);
 }
