20634c22d2736d9e7e9102b57c70aa99411b1c65
Lim Boseul <limbs722@gmail.com>
2024-07-16 17:02:12 +0900

    updates

--- level_0/최댓값_만들기.ts
+++ level_0/최댓값_만들기.ts
@@ -1,8 +1,7 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120862
 
 function makeMax2(numbers: number[]) {
-  const len = numbers.length
   const orderd = numbers.sort((a, b) => a - b);
 
-  return Math.max(orderd[0] * orderd[1], orderd[len - 1] * orderd[len - 2]);
+  return Math.max(orderd.at(0) * orderd.at(1), orderd.at(-1) * orderd.at(-2));
 }
--- level_0/캐릭터의_죄표.ts
+++ level_0/캐릭터의_죄표.ts
@@ -4,15 +4,15 @@ function movePosition(keyinput: string[], board: number[]) {
   const [x_len, y_len] = [Math.trunc(board[0] / 2), Math.trunc(board[1] / 2)];
   let pos = [0, 0];
   const move = {
-      'left': ([posX, posY]: number[]) => (posX > -x_len ? [posX - 1, posY] : [posX, posY]),
-      'right': ([posX, posY]: number[]) => (posX < x_len ? [posX + 1, posY] : [posX, posY]),
-      'up': ([posX, posY]: number[]) => (posY < y_len ? [posX, posY + 1] : [posX, posY]),
-      'down': ([posX, posY]: number[]) => (posY > -y_len ? [posX, posY - 1] : [posX, posY])
+      'left': ([posX, posY]: number[]) => ([posX > -x_len ? posX - 1 : posX, posY]),
+      'right': ([posX, posY]: number[]) => ([posX < x_len ? posX + 1 : posX, posY]),
+      'up': ([posX, posY]: number[]) => ([posX, posY < y_len ? posY + 1: posY]),
+      'down': ([posX, posY]: number[]) => ([posX, posY > -y_len ? posY - 1 : posY])
   };
   
-  keyinput.forEach((key) => {
-      pos = move[key](pos);
-  });
+  const result = keyinput.reduce((acc, value) => {
+    return move[value](acc)
+  }, pos)
   
-  return pos;
+  return result;
 }
