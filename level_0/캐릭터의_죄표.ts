f54bce281a991f5285be465fe88ce245d516a876
Lim Boseul <limbs722@gmail.com>
2024-07-22 14:49:45 +0900

    Update 캐릭터의_죄표.ts

--- level_0/캐릭터의_죄표.ts
+++ level_0/캐릭터의_죄표.ts
@@ -1,18 +1,15 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120861
 
-function movePosition(keyinput: string[], board: number[]) {
+function movePosition(keyinputs: string[], board: number[]) {
   const [x_len, y_len] = [Math.trunc(board[0] / 2), Math.trunc(board[1] / 2)];
-  let pos = [0, 0];
+  let pos: Position = [0, 0];
+
   const move = {
-      'left': ([posX, posY]: number[]) => ([posX > -x_len ? posX - 1 : posX, posY]),
-      'right': ([posX, posY]: number[]) => ([posX < x_len ? posX + 1 : posX, posY]),
-      'up': ([posX, posY]: number[]) => ([posX, posY < y_len ? posY + 1: posY]),
-      'down': ([posX, posY]: number[]) => ([posX, posY > -y_len ? posY - 1 : posY])
-  };
-  
-  const result = keyinput.reduce((acc, value) => {
-    return move[value](acc)
-  }, pos)
-  
-  return result;
+      left: ([posX, posY]) => [posX > -x_len ? posX - 1 : posX, posY],
+      right: ([posX, posY]) => [posX < x_len ? posX + 1 : posX, posY],
+      up: ([posX, posY]) => [posX, posY < y_len ? posY + 1 : posY],
+      down: ([posX, posY]) => [posX, posY > -y_len ? posY - 1 : posY],
+  } satisfies Record<string, (_: Position) => Position>;
+
+  return keyinputs.reduce((acc, keyinput) => move[keyinput](acc), pos);
 }
