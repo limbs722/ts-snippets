919d8f5f8cd53bab547c11cd78ed37b19336f6a5
Lim Boseul <limbs722@gmail.com>
2024-08-16 18:50:33 +0900

    updates: 피드백 반영

--- level_0/치킨_쿠폰.ts
+++ level_0/치킨_쿠폰.ts
@@ -1,17 +1,35 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120884
 
-function chickenCoupon(chicken: number) {
-    let ch = chicken;
+function exchangeCoupon(cp: number, exRate: number): { remainCoupons: number, addedChicken: number} {
+    const addedChicken = Math.trunc(cp / exRate);
+    const remainCoupons = cp % exRate;
+    return { remainCoupons, addedChicken}
+}
+
+function chickenCoupon(chicken: number, exchangeRate: number) {
     let coupon = chicken;
     let count = 0;
-    while (true) {
-        if (coupon < 10) {
-            break;
-        }
-        ch = Math.trunc(coupon / 10);
-        coupon = ch + (coupon % 10);
-        count += ch;
+    while (coupon >= exchangeRate) {
+        const { remainCoupons, addedChicken } = exchangeCoupon(coupon, exchangeRate);
+        count += addedChicken;
+        coupon = addedChicken + remainCoupons;
     }
     
     return count;
 }
+
+// function chickenCoupon(chicken: number) {
+//     let ch = chicken;
+//     let coupon = chicken;
+//     let count = 0;
+//     while (true) {
+//         if (coupon < 10) {
+//             break;
+//         }
+//         ch = Math.trunc(coupon / 10);
+//         coupon = ch + (coupon % 10);
+//         count += ch;
+//     }
+    
+//     return count;
+// }
