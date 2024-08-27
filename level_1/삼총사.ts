d2ec7a9461bf0d26f589758a15da95f7289bbca5
Lim Boseul <limbs722@gmail.com>
2024-08-27 21:47:58 +0900

    updates: find 함수 인자값 변경

--- level_1/삼총사.ts
+++ level_1/삼총사.ts
@@ -1,20 +1,22 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=javascript
 
 function trio(number: number[], n: number): number {
-    function find(start: number, combination: number[]): number {
+    function find(numbers: number[], combination: number[]): number {
         // combination: 현재까지의 조합
+        // numbers: 현재까지의 숫자
         if (combination.length === n) {
             return combination.reduce((sum, num) => sum + num, 0) === 0 ? 1 : 0;
         }
 
         let count = 0;
-        for (let i = start; i < number.length; i++) {
-            count += find(i + 1, [...combination, number[i]]);
+        for (let i = 0; i < numbers.length; i++) {
+            // i번째 숫자를 제외한 나머지 숫자들로 조합하기
+            count += find(numbers.slice(i + 1), [...combination, numbers[i]]);
         }
         return count;
     }
 
-    return find(0, []);
+    return find(number, []);
 }
 
 console.log(trio([-3, -2, -1, 0, 1, 2, 3], 3));
