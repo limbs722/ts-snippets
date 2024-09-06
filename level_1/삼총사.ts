7d4bfd8484ba6e17dfb55fd1e9dd00100c2cccc0
Lim Boseul <limbs722@gmail.com>
2024-09-06 14:22:09 +0900

    updates
    - 삼총사 문제 generator 이용해서 풀기

--- level_1/삼총사.ts
+++ level_1/삼총사.ts
@@ -1,22 +1,27 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=javascript
 
-function trio(number: number[], n: number): number {
-    function find(numbers: number[], combination: number[]): number {
-        // combination: 현재까지의 조합
-        // numbers: 현재까지의 숫자
-        if (combination.length === n) {
-            return combination.reduce((sum, num) => sum + num, 0) === 0 ? 1 : 0;
-        }
+function* combinationGenerator(
+    numbers: number[],
+    num: number
+): Generator<number[]> {
+    if (num == 0) yield [];
 
-        let count = 0;
-        for (let i = 0; i < numbers.length; i++) {
-            // i번째 숫자를 제외한 나머지 숫자들로 조합하기
-            count += find(numbers.slice(i + 1), [...combination, numbers[i]]);
+    for (const [idx, number] of numbers.entries()) {
+        const res = combinationGenerator(numbers.slice(idx + 1), num - 1);
+        for (const r of res) {
+            yield [number, ...r];
         }
-        return count;
     }
+}
 
-    return find(number, []);
+function trio(numbers: number[], n: number): number[][] {
+    const res = [];
+    for (const comb of combinationGenerator(numbers, n)) {
+        if (comb.reduce((acc, cur) => acc + cur, 0) === 0) {
+            res.push(comb);
+        }
+    }
+    return res;
 }
 
 console.log(trio([-3, -2, -1, 0, 1, 2, 3], 3));
