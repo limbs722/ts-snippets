2200acd3500ecf477ea0376f01c8057170d9e56f
Lim Boseul <limbs722@gmail.com>
2024-11-04 13:37:38 +0900

    updates - 주석제거

--- level_1/삼총사.ts
+++ level_1/삼총사.ts
@@ -1,27 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=javascript
 
-// function* combinationGenerator(
-//     numbers: number[],
-//     num: number
-// ): Generator<number[]> {
-//     if (num == 0) yield [];
-
-//     for (const [idx, number] of numbers.entries()) {
-//         const res = combinationGenerator(numbers.slice(idx + 1), num - 1);
-//         for (const r of res) {
-//             yield [number, ...r];
-//         }
-//     }
-// }
-
-// function* trioGenerator(numbers: number[], n: number): Generator<number[]> {
-//     for (const comb of combinationGenerator(numbers, n)) {
-//         if (comb.reduce((acc, cur) => acc + cur, 0) === 0) {
-//             yield comb;
-//         }
-//     }
-// }
-
 function* combinationGenerator(numbers: number[], num: number, current: number[] = []): Generator<number[]> {
     if (num === 0) {
         if (current.reduce((acc, cur) => acc + cur, 0) === 0) {
